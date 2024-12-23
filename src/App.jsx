3277d68da6d0d5d4d42d865df10b27967847344f
Ayodeji <ayodejisamuel80@gmail.com>
2024-12-23 18:46:25 +0100

    new additions

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,25 @@ import { ChakraProvider, Box } from "@chakra-ui/react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/Homepage/HomePage";
 import AuthPage from "./Pages/AuthPage/AuthPage";
+import PageLayout from "./Layout/PageLayout/PageLayout";
 import "./App.css";
 
 function App() {
   return (
     <ChakraProvider>
+ 
       <Router basename="/instagg">
+      <PageLayout>
         <Box>
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/auth" element={<AuthPage />} />
           </Routes>
         </Box>
+        </PageLayout>
       </Router>
+  
+ 
     </ChakraProvider>
   );
 }
---
+++ src/Layout/PageLayout/PageLayout.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Flex, Box } from "@chakra-ui/react";
+import Sidebar from "../../component/Sidebar/Sidebar";
+import { useLocation } from "react-router-dom";
+
+const PageLayout = ({ children }) => {
+    const {pathname} =  useLocation()
+  return (
+    <div>
+      <Flex>
+      {pathname !== '/auth' ? (
+        <Box width={{base: '70px', md:'240px'}}>
+          <Sidebar />
+        </Box>
+      ) : null}
+      
+
+        <Box flex={1} width={{base: "calc(100% - 70px)", md: 'calc(100% - 240%)'}}>{children}</Box>
+      </Flex>
+    </div>
+  );
+};
+
+export default PageLayout;
