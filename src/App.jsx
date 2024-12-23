29d43e4511ef462f320299344ecf4aafc65d2942
Ayodeji <ayodejisamuel80@gmail.com>
2024-12-23 08:57:06 +0100

    auth structuring

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { ChakraProvider, Box, Button, HStack } from '@chakra-ui/react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './Pages/Homepage/HomePage';
+import AuthPage from './Pages/AuthPage/AuthPage';
+import './App.css';
+
+function App() {
+  return (
+    <ChakraProvider>
+      <Router>
+        <Box p={4}>
+          {/* <HStack spacing={4}>
+            <Button colorScheme="teal">Home</Button>
+            <Button colorScheme="teal">Auth</Button>
+          </HStack> */}
+
+          {/* Define routes */}
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/auth" element={<AuthPage />} />
+          </Routes>
+        </Box>
+      </Router>
+    </ChakraProvider>
+  );
+}
+
+export default App;
---
+++ src/Pages/Homepage/HomePage.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Box, Container, Flex, VStack, Image } from "@chakra-ui/react";
+import { base } from "framer-motion/client";
+import AuthForm from "../../component/AuthForm/AuthForm";
+
+const HomePage = () => {
+  return (
+    <>
+      <Flex
+        maxH={"100vh"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        px={4}
+      >
+        <Container maxW={"container.md"} padding={0}>
+          <Flex>
+            <Box display={{ base: "none", md: "block" }}>
+              <Image src="/auth.png" alt="phone-img" />
+            </Box>
+            <VStack>
+              <AuthForm />
+              <Box>Get the App</Box>
+              <Flex justifyContent={"center"} alignContent={"center"} gap={5}>
+                <Image src="/microsoft.png" h={"10"} alt={"playstore logo"} />
+                <Image src='/playstore.png' h={"10"} alt={"playstore logo"} />
+              </Flex>
+            </VStack>
+          </Flex>
+        </Container>
+      </Flex>
+    </>
+  );
+};
+
+export default HomePage;
