f1ef33e57239b156aeab1752e35c10c150ba6653
Ayodeji <ayodejisamuel80@gmail.com>
2024-12-23 19:39:06 +0100

    new additions

--- src/Layout/PageLayout/PageLayout.tsx
+++ src/Layout/PageLayout/PageLayout.tsx
@@ -4,18 +4,22 @@ import Sidebar from "../../component/Sidebar/Sidebar";
 import { useLocation } from "react-router-dom";
 
 const PageLayout = ({ children }) => {
-    const {pathname} =  useLocation()
+  const { pathname } = useLocation();
   return (
     <div>
       <Flex>
-      {pathname !== '/auth' ? (
-        <Box width={{base: '70px', md:'240px'}}>
-          <Sidebar />
-        </Box>
-      ) : null}
-      
+        {pathname !== "/auth" ? (
+          <Box width={{ base: "70px", md: "240px" }}>
+            <Sidebar />
+          </Box>
+        ) : null}
 
-        <Box flex={1} width={{base: "calc(100% - 70px)", md: 'calc(100% - 240%)'}}>{children}</Box>
+        <Box
+          flex={1}
+          width={{ base: "calc(100% - 70px)", md: "calc(100% - 240%)" }}
+        >
+          {children}
+        </Box>
       </Flex>
     </div>
   );
