b6697f3ec154c1199c71226ea0a3f8535d1797d3
Ayodeji <ayodejisamuel80@gmail.com>
2024-12-25 03:11:25 +0100

    loading state ui ;

--- src/component/FeedPosts/FeedData.jsx
+++ src/component/FeedPosts/FeedData.jsx
@@ -1,10 +1,83 @@
-import FeedPosts from "./FeedPosts"
-const FeedData = () => {
-    return (<>
-    <FeedPosts  username='determinant'  img='/instagg/profilepic.jpg' avatar='/instagg/profilepic.jpg'  />
-    <FeedPosts   username='ishola_faith10' img='/instagg/img3.png' avatar='/instagg/img3.png'/>
+import { useEffect, useState } from "react";
+import FeedPosts from "./FeedPosts";
+import {
+  VStack,
+  Flex,
+  Container,
+  SkeletonCircle,
+  SkeletonText,
+  Skeleton,
+  Box,
+} from "@chakra-ui/react";
 
-    <FeedPosts   username='maverick' img='/instagg/img2.png' avatar='/instagg/img2.png'/>
-    </>)
-}
-export default FeedData
+const FeedData = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const posts = [
+    {
+      username: "johndoe",
+      img: "/instagg/img4.png",
+      avatar: "/instagg/img4.png",
+    },
+    {
+      username: "determinant_",
+      img: "/instagg/profilepic.jpg",
+      avatar: "/instagg/profilepic.jpg",
+    },
+    {
+      username: "ishola_faith10",
+      img: "/instagg/img3.png",
+      avatar: "/instagg/img3.png",
+    },
+    {
+      username: "maverick_",
+      img: "/instagg/img2.png",
+      avatar: "/instagg/img2.png",
+    },
+  ];
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
+  return (
+    <Container maxW="container.lg" py={4}>
+      {isLoading
+        ? [0, 1, 2, 3].map((_, index) => (
+            <VStack key={index} gap={4} alignItems="flex-start" mb={6}>
+              <Flex
+                alignItems="center"
+                justifyContent="space-between"
+                gap={2}
+                p={6}
+                boxShadow="lg"
+                w="full"
+              >
+                <Flex gap={2} alignItems="center">
+                  {" "}
+                  <SkeletonCircle size="10" />
+                  <Skeleton height="10px" width="200px" />
+                </Flex>
+
+                <Skeleton height="10px" width="50px" />
+              </Flex>
+              <Skeleton w="full">
+                <Box h="300px">Contents wrapped</Box>
+              </Skeleton>
+            </VStack>
+          ))
+        : posts.map((post, index) => (
+            <FeedPosts
+              key={index}
+              username={post.username}
+              img={post.img}
+              avatar={post.avatar}
+            />
+          ))}
+    </Container>
+  );
+};
+
+export default FeedData;
--- src/component/FeedPosts/FeedPosts.jsx
+++ src/component/FeedPosts/FeedPosts.jsx
@@ -2,11 +2,11 @@ import PostHeader from "./PostHeader";
 import PostFooter from './PostFooter';
 import { Avatar, Image } from '@chakra-ui/react';
 
-const FeedPosts = ({avatar, username, img}) => {
+const FeedPosts = ({avatar, username, img, alt}) => {
     return (
         <>
             <PostHeader  avatar={avatar}  username={username}/>
-            <Image w={'full'} borderRadius={4} overflow={'hidden'} src={img} />
+            <Image w={'full'} borderRadius={4} alt={alt} overflow={'hidden'} src={img} />
             <PostFooter username={username}/>
         </>
 
