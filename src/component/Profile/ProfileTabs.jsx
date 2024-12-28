30c6706410da52f7b7433b082583f636d6f26fb8
Ayodeji <ayodejisamuel80@gmail.com>
2024-12-28 14:40:00 +0100

    userpost ui

--- src/component/Profile/ProfileTabs.jsx
+++ src/component/Profile/ProfileTabs.jsx
@@ -1,8 +1,62 @@
-const ProfileTabs = () => {
+import { Flex, Text, Box } from "@chakra-ui/react";
+import { BsGrid3X3 } from "react-icons/bs";
 
-    return (<div>
-        Profile Tabs
-    </div>)
-}
+const ProfileTabs = () => {
+  return (
+    <Flex
+      w={"full"}
+      justifyContent={"center"}
+      gap={{ base: 4, sm: 10 }}
+      textTransform={"uppercase"}
+      fontWeight={"bold"}
+      color={'white'}
+    >
+      <Flex
+        borderTop={"1px solid white"}
+        p="3"
+        gap={1}
+        alignItems={"center"}
+        cursor={"pointer"}
+      >
+        <Box>
+          <BsGrid3X3 />
+        </Box>
+        <Text fontSize={12} display={{ base: "none", sm: "block" }}>
+          Posts
+        </Text>
+      </Flex>
+
+      <Flex
+        borderTop={"1px solid white"}
+        p="3"
+        gap={1}
+        alignItems={"center"}
+        cursor={"pointer"}
+      >
+        <Box>
+          <BsGrid3X3 />
+        </Box>
+        <Text fontSize={12} display={{ base: "none", sm: "block" }}>
+          Saved
+        </Text>
+      </Flex>
+
+      <Flex
+        borderTop={"1px solid white"}
+        p="3"
+        gap={1}
+        alignItems={"center"}
+        cursor={"pointer"}
+      >
+        <Box>
+          <BsGrid3X3 />
+        </Box>
+        <Text fontSize={12} display={{ base: "none", sm: "block" }}>
+          Likes
+        </Text>
+      </Flex>
+    </Flex>
+  );
+};
 
 export default ProfileTabs;
