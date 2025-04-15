ad3b9c8a2ab69d5683504594f36c35835e00c2d8
Ayodeji <ayodejisamuel80@gmail.com>
2025-04-15 23:00:18 +0100

    new changes

--- src/component/Sidebar/Search.jsx
+++ src/component/Sidebar/Search.jsx
@@ -1,40 +1,94 @@
-import React from "react";
-import { Tooltip,Box, Text  } from "@chakra-ui/react";
-import {  SearchLogo} from "../../assets/constants";
-import  {Link }  from "react-router-dom";
+import React, { useRef } from "react";
+import {
+  Tooltip,
+  Box,
+  Text,
+  useDisclosure,
+  Modal,
+  ModalBody,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  FormControl,
+  FormLabel,
+  Input,
+  Flex,
+  Button,
+} from "@chakra-ui/react";
+import { SearchLogo } from "../../assets/constants";
+import SearchUser from "../../hooks/useSearchUser";
 
-const  Search = () => {
+const Search = () => {
+  const { isOpen, onClose, onOpen } = useDisclosure();
+  const { user, getUserProfile, loading } = SearchUser();
+  const searchRef = useRef(null);
 
-return (
+  const handleSearchUser = (e) => {
+    e.preventDefault();
+    if (searchRef.current && searchRef.current.value.trim()) {
+      getUserProfile(searchRef.current.value.trim());
+    }
+  };
+  console.log(user);
+
+  return (
     <div>
-<Tooltip
-             label={'Notifications'}
-              placement="right"
-              openDelay={500}
-              hasArrow
-              ml={1}
-              display={{ base: "block", md: "none" }}
-            >
-              <Box
-                as={Link}
-                to={"/nofications"}
-                display="flex"
-                justifyContent={{ base: "center", md: "left" }}
-                alignItems="center"
-                gap={4}
-                _hover={{ bg: "whiteAlpha.400" }}                borderRadius={6}
-                p={2}
-              >
- <SearchLogo />
- 
-                <Text display={{ base: "none", md: "block" }} fontSize="sm">
+      <Tooltip
+        label={"Search"}
+        placement="right"
+        openDelay={500}
+        hasArrow
+        ml={1}
+        display={{ base: "block", md: "none" }}
+      >
+        <Box
+          as="button"
+          onClick={onOpen}
+          display="flex"
+          justifyContent={{ base: "center", md: "left" }}
+          alignItems="center"
+          gap={4}
+          _hover={{ bg: "whiteAlpha.400" }}
+          borderRadius={6}
+          p={2}
+        >
+          <SearchLogo />
+          <Text display={{ base: "none", md: "block" }} fontSize="sm">
+            Search
+          </Text>
+        </Box>
+      </Tooltip>
+
+      <Modal isOpen={isOpen} onClose={onClose} motionPreset="slideInBottom">
+        <ModalOverlay />
+        <ModalContent bg="black" border="1px solid gray" maxW="400px">
+          <ModalHeader>Search User</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <form onSubmit={handleSearchUser}>
+              <FormControl>
+                <FormLabel>Username</FormLabel>
+                <Input placeholder="Enter username" ref={searchRef} />
+              </FormControl>
+              <Flex w="full" justifyContent="flex-end">
+                <Button
+                  type="submit"
+                  ml="auto"
+                  size="sm"
+                  my={4}
+                  isLoading={loading}
+                  colorScheme="blue"
+                >
                   Search
-                </Text>
-              </Box>
-            </Tooltip>        
+                </Button>
+              </Flex>
+            </form>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </div>
-)
-}
+  );
+};
 
-
-export default Search ;
+export default Search;
